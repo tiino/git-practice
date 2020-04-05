ed726eb17bf422cd137fefebcc65e1a2e370dc32
tiino <t.iino1103@gmail.com>
2020-04-05 16:05:26 +0900

    テスト用クラスの追加

---
+++ calc.py
@@ -0,0 +1,11 @@
+class Calc():
+    def __init__(self):
+        pass
+    
+    def calc_sum(self, a, b):
+        return a + b
+    
+    def calc_div(self, a, b):
+        return a / b
+    
+    
